34a6c0429c694641d92c78eec4654f0f3114eeab
Rohailtools <rohailiqbal923@gmail.com>
2026-07-13 12:19:27 +0500

    Update config for environment variables

--- app/config.py
+++ app/config.py
@@ -1,14 +1,24 @@
 from pydantic_settings import BaseSettings
 
+
 class Settings(BaseSettings):
 
-    APP_NAME: str = "FastFood AI v2"
+    APP_NAME: str = "FastFood AI"
 
     APP_VERSION: str = "2.0.0"
 
     DEBUG: bool = True
 
+    SUPABASE_URL: str
+
+    SUPABASE_ANON_KEY: str
+
+    SUPABASE_SERVICE_KEY: str
+
+    GROQ_API_KEY: str = ""
+
     class Config:
         env_file = ".env"
+
 
 settings = Settings()
